4d84ac4cb0f63c9c907a28ed28b856a53eaa7deb
CollinsKfe <collinsikwebe@gmail.com>
2023-08-15 11:22:54 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
- *
- * Return: 0 (success)
+ * print_alphabet - Make the alphabet
+ * Return: void
  */
 void print_alphabet(void)
 {
-	int i;
+	char c;
 	
-	for (i = 97; i <= 122; i++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
